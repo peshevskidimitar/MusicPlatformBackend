037cfc058d579258ca977276904571fc39e9b99d
peshevskidimitar <pesevskidimitar@gmail.com>
2022-12-15 13:47:26 +0100

    Initial commit

---
+++ music-platform-backend/src/main/java/mk/fcse/musicplatformbackend/MusicPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package mk.fcse.musicplatformbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicPlatformBackendApplication.class, args);
+    }
+
+}
