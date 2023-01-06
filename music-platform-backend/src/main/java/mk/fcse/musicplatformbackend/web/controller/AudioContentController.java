b6d04c903ad0dcf1afcda4e34b48cb8df28b58b3
peshevskidimitar <pesevskidimitar@gmail.com>
2023-01-06 10:54:41 +0100

    Update AudioContentController

--- music-platform-backend/src/main/java/mk/fcse/musicplatformbackend/web/controller/AudioContentController.java
+++ music-platform-backend/src/main/java/mk/fcse/musicplatformbackend/web/controller/AudioContentController.java
@@ -4,11 +4,13 @@ import mk.fcse.musicplatformbackend.model.stats.MonthlyReviewsAndAverageGradePer
 import mk.fcse.musicplatformbackend.service.AudioContentService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/audio-contents")
+@RestController
+@RequestMapping(("/audio-contents"))
 @CrossOrigin
 public class AudioContentController {
 
